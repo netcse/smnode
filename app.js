4d1716e4c0b99a65f52e5fcd5c05f27ed5d06791
netcse <smamran09@gmail.com>
2017-10-24 20:06:06 +0600

    initial commit

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var http = require('http');
 
 var PORT = 80, HOST = '0.0.0.0';
 
+
 var server = http.createServer(function (req, res, next) {
-    res.end('<script>alert("I am from JS");</script>');
+    res.end('<iframe width="420" height="345" src="https://www.youtube.com/embed/CEr8s1ljpYY">\n' +
+        '</iframe>');
+
+    var httptwo = require('./httptwo')();
+
 }).listen(PORT, HOST, function () {
     console.log('Server Listening on port : ' + PORT);
 });
--- httptwo.js
+++ httptwo.js
@@ -1,52 +1,58 @@
-const http = require('http');
-const net = require('net');
-const url = require('url');
+module.exports = function () {
+    function httpRequest() {
+        const http = require('http');
+        const net = require('net');
+        const url = require('url');
 
 // Create an HTTP tunneling proxy
-const proxy = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('okay');
-});
-proxy.on('connect', (req, cltSocket, head) => {
-    // connect to an origin server
-    const srvUrl = url.parse(`http://${req.url}`);
-    const srvSocket = net.connect(srvUrl.port, srvUrl.hostname, () => {
-        cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
-            'Proxy-agent: Node.js-Proxy\r\n' +
-            '\r\n');
-        srvSocket.write(head);
-        srvSocket.pipe(cltSocket);
-        cltSocket.pipe(srvSocket);
-    });
-});
+        const proxy = http.createServer((req, res) => {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('okay');
+        });
+        proxy.on('connect', (req, cltSocket, head) => {
+            // connect to an origin server
+            const srvUrl = url.parse(`http://${req.url}`);
+            const srvSocket = net.connect(srvUrl.port, srvUrl.hostname, () => {
+                cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+                    'Proxy-agent: Node.js-Proxy\r\n' +
+                    '\r\n');
+                srvSocket.write(head);
+                srvSocket.pipe(cltSocket);
+                cltSocket.pipe(srvSocket);
+            });
+        });
 
 // now that proxy is running
-proxy.listen(1337, '127.0.0.1', () => {
+        proxy.listen(1333, '127.0.0.1', () => {
 
-    // make a request to a tunneling proxy
-    const options = {
-        port: 1337,
-        hostname: '127.0.0.1',
-        method: 'CONNECT',
-        path: 'www.google.com:80'
-    };
+            // make a request to a tunneling proxy
+            const options = {
+                port: 1333,
+                hostname: '127.0.0.1',
+                method: 'CONNECT',
+                path: 'www.google.com:80'
+            };
 
-    const req = http.request(options);
-    req.end();
+            const req = http.request(options);
+            req.end();
 
-    req.on('connect', (res, socket, head) => {
-        console.log('got connected!');
+            req.on('connect', (res, socket, head) => {
+                console.log('got connected!');
 
-        // make a request over an HTTP tunnel
-        socket.write('GET / HTTP/1.1\r\n' +
-            'Host: www.google.com:80\r\n' +
-            'Connection: close\r\n' +
-            '\r\n');
-        socket.on('data', (chunk) => {
-            console.log(chunk.toString());
+                // make a request over an HTTP tunnel
+                socket.write('GET / HTTP/1.1\r\n' +
+                    'Host: www.google.com:80\r\n' +
+                    'Connection: close\r\n' +
+                    '\r\n');
+                socket.on('data', (chunk) => {
+                    console.log(chunk.toString());
+                });
+                socket.on('end', () => {
+                    proxy.close();
+                });
+            });
         });
-        socket.on('end', () => {
-            proxy.close();
-        });
-    });
-});
+    }
+
+    httpRequest();
+};
